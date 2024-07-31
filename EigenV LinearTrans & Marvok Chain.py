d6aaf25d8fc0698ec106b12693d24f5b8de010a8
Zhaojie Wang <332190380@qq.com>
2024-07-31 18:21:57 +0800

    Update EigenV LinearTrans & Marvok Chain.py

--- EigenV LinearTrans & Marvok Chain.py
+++ EigenV LinearTrans & Marvok Chain.py
@@ -44,43 +44,31 @@ print(X)
 # It is useful to predict the probabilities in Xm when m is large, and thus determine what pages a browser is more likely to visit after a long period of browsing the web. In other words, we want to know which pages ultimately get the most traffic. One way to do that is just apply the transformation many times, and with this small $5 \times 5$ example you can do that just fine. In real life problems, however, you'll have enormous matrices and doing so will be computationally expensive. Here is where eigenvalues and eigenvectors can help here significantly reducing the amount of calculations. Let's see how!
 不断增加m，m=50, m=5000, m=50000, 最后出现 Xm+1 = Xm，系统进入稳态
 这种稳态，表示为: 有 Vn+1 = Vn, 有P * Vn = 1 * Vn+1，则 Vn或Vn+1为Eigenvector with Eigenvalue = 1。 即稳态的Vn是转化矩阵P的Eigenvector以Eigenvalue为1。
-
+###必须是Markov Matrix 才有这样的性质###
 
 # Begin by finding eigenvalues and eigenvectors for the previously defined matrix P：
 eigenvals, eigenvecs = np.linalg.eig(P)
 print(f'Eigenvalues of P:\n{eigenvals}\n\nEigenvectors of P\n{eigenvecs}')
 
 
-# As you can see, there is one eigenvalue with value $1$, and the other four have an aboslute values smaller than 1. It turns out this is a property of transition matrices. In fact, they have so many properties that these types of matrices fall into a category of matrices called **Markov matrix**. 
-# 
-# In general, a square matrix whose entries are all nonnegative, and the sum of the elements for each column is equal to $1$ is called a **Markov matrix**. Markov matrices have a handy property - they always have an eigenvalue equal to 1. As you learned in the lectures, in the case of transition matrices, the eigenvector associated with the eigenvalue $1$ will determine the state of the model in the long run , after evolving for a long period of time. 
-# 
+# As you can see, there is one eigenvalue with value $1$, and the other four have an aboslute values smaller than 1. It turns out this is a property of transition matrices. In fact, they have so many properties that these types of matrices fall into a category of matrices called “Markov matrix”. 
+# In general, a square matrix whose entries are all nonnegative, and the sum of the elements for each column is equal to 1 is called a Markov matrix. 
+# Markov matrices have a handy property - they always have an eigenvalue equal to 1. 
 # You can easily verify that the matrix P you defined earlier is in fact a Markov matrix. 
-# So, if m is large enough, the equation Xm+1 =P @ Xm can be rewritten as Xm+1=P@Xm-1=1\times X_m$. This means that predicting probabilities at time $m$, when $m$ is large you can simply just look for an eigenvector corresponding to the eigenvalue $1$. 
-# 
-# So, let's extract the eigenvector associated to the eigenvalue $1$. 
-
-# In[37]:
-
+# So, if  𝑚 is large enough, the equation  𝑋𝑚=𝑃𝑋𝑚−1
+  can be rewritten as  𝑋𝑚=𝑃𝑋𝑚−1=1×𝑋𝑚
+  This means that predicting probabilities at time  𝑚 , when  𝑚 is large you can simply just look for an eigenvector corresponding to the eigenvalue  1
+# So, let's extract the eigenvector associated to the eigenvalue 1. 
 
 X_inf = eigenvecs[:,0]
 
 print(f"Eigenvector corresponding to the eigenvalue 1:\n{X_inf[:,np.newaxis]}")
 
+Just to verify the results:
+perform matrix multiplication  𝑃𝑋 (multiply matrix P and vector X_inf) to check that the result will be equal to the vector  𝑋 (X_inf).
 
-# <a name='ex02'></a>
-# ### Exercise 2
-# 
-# Just to verify the results, perform matrix multiplication $PX$ (multiply matrix `P` and vector `X_inf`) to check that the result will be equal to the vector $X$ (`X_inf`).
-
-# In[38]:
-
-
-# This is organised as a function only for grading purposes.
 def check_eigenvector(P, X_inf):
-    ### START CODE HERE ###
     X_check = P @ X_inf
-    ### END CODE HERE ###
     return X_check
 
 X_check = check_eigenvector(P, X_inf)
@@ -89,13 +77,6 @@ print("Result of multiplication:" + str(X_check))
 
 # Function np.isclose compares two NumPy arrays element by element, allowing for error tolerance (rtol parameter).
 print("Check that PX=X element by element:" + str(np.isclose(X_inf, X_check, rtol=1e-10)))
-
-
-# In[39]:
-
-
-# Test your solution.
-w4_unittest.test_check_eigenvector(check_eigenvector)
 
 
 # This result gives the direction of the eigenvector, but as you can see the entries can't be interpreted as probabilities since you have negative values, and they don't add to 1. That's no problem. Remember that by convention `np.eig` returns eigenvectors with norm 1, but actually any vector on the same line is also an eigenvector to the eigenvalue 1, so you can simply scale the vector so that all entries are positive and add to one.This will give you the long-run probabilities of landing on a given web page.
